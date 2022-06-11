5909026cbe4faae2521d73d2f2aa338224b36922
ichenadamczyk20 <ichenadamczyk20@stuy.edu>
2022-06-11 11:50:40 -0400

    level logic

--- scripts/screens.js
+++ scripts/screens.js
@@ -1,28 +1,60 @@
 var levelSelect = document.querySelector("#level-select");
 var winScreen = document.querySelector("#win-screen");
 var loseScreen = document.querySelector("#lose-screen");
+var currentLevel;
+
 levelSelect.style.display = "block";
 
 gameStatus = "level-select";
 
 // functions for level select buttons
 function callLevel(num) {
+    if (SAVE_DATA[num-2] && !SAVE_DATA[num - 2].completed) return;
     levelSelect.style.display = "none";
     gameStatus = "loading";
     loadLevel(num - 1);
+    currentLevel = num - 1;
 }
 
 function win() {
+    document.querySelector("#coins-end").innerHTML = "Coins: " + CTX_level.coinsCnt;
+    var timePoints = parseInt(Math.max(0, SAVE_DATA[currentLevel].maxtime - timeNow(startTime) / 1000));
+    var accPoints = CTX_level.points + timePoints * 10;
+    document.querySelector("#points-end").innerHTML = "Points: " + accPoints;
+    SAVE_DATA[currentLevel].highscore = Math.max(SAVE_DATA[currentLevel].highscore, accPoints)
+    document.querySelector("#highscore-end").innerHTML = "Highscore: " + SAVE_DATA[currentLevel].highscore;
+    document.querySelector("#time-end").innerHTML = document.querySelector("#timer").innerHTML;
+
     winScreen.style.display = "block";
     document.querySelector("#music").pause();
-    // gameStatus = "win";
+    SAVE_DATA[currentLevel].completed = true;
+    var nextLevelBtn = document.querySelector("#next-level2");
+    if (SAVE_DATA[currentLevel].completed && currentLevel + 1 < 10) nextLevelBtn.style.display = "block";
+    else nextLevelBtn.style.display = "none";
+
+    nextLevelBtn.onclick = "callLevel(" + currentLevel + ")";
+
+    var levelElem = document.getElementsByClassName("level")[currentLevel + 1];
+    if (levelElem) levelElem.classList.remove("level-disabled");
 }
 
 function lose() {
+    document.querySelector("#coins-end2").innerHTML = "Coins: " + CTX_level.coinsCnt;
+    var timePoints = parseInt(Math.max(0, SAVE_DATA[currentLevel].maxtime - timeNow(startTime) / 1000));
+    var accPoints = CTX_level.points + timePoints * 10;
+    document.querySelector("#points-end2").innerHTML = "Points: " + accPoints;
+    SAVE_DATA[currentLevel].highscore = Math.max(SAVE_DATA[currentLevel].highscore, accPoints)
+    document.querySelector("#highscore-end2").innerHTML = "Highscore: " + SAVE_DATA[currentLevel].highscore;
+    document.querySelector("#time-end2").innerHTML = document.querySelector("#timer").innerHTML;
     //losescreen
     // alert("executed");
     loseScreen.style.display = "block";
     document.querySelector("#music").pause();
+    var nextLevelBtn = document.querySelector("#next-level2");
+    if (SAVE_DATA[currentLevel].completed && currentLevel + 1 < 10) nextLevelBtn.style.display = "block";
+    else nextLevelBtn.style.display = "none";
+
+    nextLevelBtn.onclick = "callLevel(" + currentLevel + ")";
 }
 
 function restartButton() {
@@ -31,7 +63,7 @@ function restartButton() {
     // alert("executed");
     winScreen.style.display = "none";
     loseScreen.style.display = "none";
-    levelSelect.style.display = "block";
+    callLevel(currentLevel + 1);
 }
 let btn = document.getElementById("restart");
 btn.addEventListener('click', event => {
@@ -41,6 +73,31 @@ let btn2 = document.getElementById("restart2");
 btn2.addEventListener('click', event => {
     restartButton();
 });
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'r' || event.key === 'R') {
+        restartButton();
+    }
+});
+
+function selectLevel() {
+    // document.querySelector('#level-select').style.display = "block";
+    // console.log("restart");
+    // alert("executed");
+    winScreen.style.display = "none";
+    loseScreen.style.display = "none";
+    levelSelect.style.display = "block";
+}
+
+
+btn = document.getElementById("select-level");
+btn.addEventListener('click', event => {
+    restartButton();
+});
+btn2 = document.getElementById("select-level2");
+btn2.addEventListener('click', event => {
+    restartButton();
+});
+
 
 
 // pseudo-language
@@ -93,4 +150,47 @@ LEVEL_DATA = [
     //     new PointCharge(400, 150, -0.00025),
     // ], new Winbox(210, 120, 50, 50)),
     // {},
+];
+
+SAVE_DATA = [
+    //1
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //2
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //3
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //4
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //5
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //6
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //7
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //8
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //9
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
+    //10
+    {completed: false,
+    highscore: 0,
+    maxtime: 120},
 ];
